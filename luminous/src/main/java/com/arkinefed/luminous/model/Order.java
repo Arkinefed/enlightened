666405381d6eaedd848cf7eefa9e08a93fe644c6
Arkinefed <arkinefedmail@gmail.com>
2023-05-03 19:17:40 +0200

    l, added functions in Order class

--- luminous/src/main/java/com/arkinefed/luminous/model/Order.java
+++ luminous/src/main/java/com/arkinefed/luminous/model/Order.java
@@ -15,4 +15,45 @@ public class Order {
     private User user;
     @ManyToMany
     private List<SamplePack> samplePacks;
+
+    public Order() {
+    }
+
+    public Order(LocalDateTime date, User user, List<SamplePack> samplePacks) {
+        this.date = date;
+        this.user = user;
+        this.samplePacks = samplePacks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<SamplePack> getSamplePacks() {
+        return samplePacks;
+    }
+
+    public void setSamplePacks(List<SamplePack> samplePacks) {
+        this.samplePacks = samplePacks;
+    }
 }
