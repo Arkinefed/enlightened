ae6ea89e6356e62c355a77f8554b6552bfea597a
Arkinefed <arkinefedmail@gmail.com>
2023-05-10 11:18:39 +0200

    e, displaying list of sample packs

--- enlightened/src/components/sample-packs/SamplePackInformations.vue
+++ enlightened/src/components/sample-packs/SamplePackInformations.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="sample-pack-information">
-
-    </div>
+	<div class="sample-pack-information">
+		<div>{{ id }}</div>
+		<div>{{ name }}</div>
+		<div>{{ price }}</div>
+		<div>{{ genreName }}</div>
+		<div>{{ description }}</div>
+		<div>{{ releaseDate }}</div>
+	</div>
 </template>
 
 <script>
 export default {
-
+	props: [
+		'id',
+		'name',
+		'price',
+		'genreName',
+		'description',
+		'releaseDate'
+	]
 }
 </script>
 
--- enlightened/src/views/SamplePacksView.vue
+++ enlightened/src/views/SamplePacksView.vue
@@ -1,13 +1,43 @@
 <template>
 	<div class="sample-packs">
-
+		<template v-if="dataReceived">
+			<sample-pack-informations v-for="pack in samplePacks" v-bind:key="pack.id" :id="pack.id" :name="pack.name"
+				:price="pack.price" :genreName="pack.genreName" :description="pack.description"
+				:releaseDate="pack.releaseDate" />
+		</template>
+		<h2 v-else>{{ message }}</h2>
 	</div>
 </template>
 
 <script>
-export default {
-	mounted() {
+import SamplePackInformations from '@/components/sample-packs/SamplePackInformations.vue'
+import axios from 'axios'
 
+export default {
+	components: {
+		SamplePackInformations
+	},
+	data() {
+		return {
+			dataReceived: false,
+			message: 'loading data',
+			samplePacks: []
+		}
+	},
+	mounted() {
+		this.getAllSamplePacks()
+	},
+	methods: {
+		getAllSamplePacks() {
+			axios.get('http://localhost:8080/resource/sample-pack/all')
+				.then(response => {
+					this.samplePacks = response.data
+					this.dataReceived = true
+				})
+				.catch(error => {
+					this.message = error.message
+				})
+		}
 	}
 }
 </script>
