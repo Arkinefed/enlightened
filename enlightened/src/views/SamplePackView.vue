dc1258b6e51dee638612658f9efe913d0005bd82
Arkinefed <arkinefedmail@gmail.com>
2023-06-03 23:30:43 +0200

    e, get sample pack information

--- enlightened/src/views/SamplePackView.vue
+++ enlightened/src/views/SamplePackView.vue
@@ -1,10 +1,39 @@
 <template>
-    <div id="sample-pack">
-        {{ $route.params.id }}
-    </div>
+	<div id="sample-pack">
+		<template v-if="dataReceived">
+			<h1>{{ samplePack.name }}</h1>
+		</template>
+		<h2 v-else>{{ message }}</h2>
+	</div>
 </template>
 
 <script>
+import axios from 'axios'
+
+export default {
+	data() {
+		return {
+			dataReceived: false,
+			message: 'loading data',
+			samplePack: null
+		}
+	},
+	mounted() {
+		this.getSamplePack(this.$route.params.id)
+	},
+	methods: {
+		getSamplePack(id) {
+			axios.get('http://localhost:8080/resource/sample-pack/id/' + id)
+				.then(response => {
+					this.samplePack = response.data
+					this.dataReceived = true
+				})
+				.catch(error => {
+					this.message = error.message
+				})
+		}
+	}
+}
 </script>
 
 <style></style>
